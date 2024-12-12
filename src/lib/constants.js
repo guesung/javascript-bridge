cac1cc23b59cdb5338a6cc933751ac01ebd306fe
guesung <gueit214@naver.com>
2024-12-12 21:08:24 +0900

    feat: 초기 세팅

---
+++ src/lib/constants.js
@@ -0,0 +1,24 @@
+export const INPUT_MESSAGE = {
+  bridgeLength: {
+    default: () => `값을 입력해주세요.\n`,
+  },
+  example2: {
+    default: () => `값을 입력해주세요.\n`,
+  },
+  example3: {
+    default: () => `값을 입력해주세요.\n`,
+  },
+};
+
+export const OUTPUT_MESSAGE = {
+  ex1: () => `\n결과: `,
+  ex2: () => `\n결과: `,
+  ex3: () => `\n결과: `,
+};
+
+export const ERROR_MESSAGE_DEFAULT = '[ERROR]';
+export const ERROR_MESSAGE = {
+  example: `${ERROR_MESSAGE_DEFAULT} 예시를 입력해주세요!`,
+};
+
+export const SEPARATER = ',';
