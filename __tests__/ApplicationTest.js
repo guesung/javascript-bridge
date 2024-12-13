19a884c13b0351909ca3bd9530002b2b0e077568
guesung <gueit214@naver.com>
2024-12-13 12:46:16 +0900

    test: 예외 케이스 추가 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,6 +1,7 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const App = require('../src/App');
 const BridgeMaker = require('../src/BridgeMaker');
+const { ERROR_MESSAGE } = require('../src/lib/constants.js');
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -31,14 +32,16 @@ const expectLogContains = (received, logs) => {
   });
 };
 
-const runException = async (inputs) => {
-  mockQuestions(inputs);
+const INPUTS_FOR_END = ['3', 'U', 'U', 'U', 'Q'];
+
+const runException = async (inputs, errorMessage) => {
+  mockQuestions([...inputs, ...INPUTS_FOR_END]);
   const logSpy = getLogSpy();
   const app = new App();
 
   await app.play();
 
-  expectLogContains(getOutput(logSpy), ['[ERROR]']);
+  expectLogContains(getOutput(logSpy), [errorMessage]);
 };
 
 const expectBridgeOrder = (received, upside, downside) => {
@@ -52,19 +55,15 @@ describe('다리 건너기 테스트', () => {
   test('다리 생성 테스트', () => {
     const randomNumbers = ['1', '0', '0'];
     const mockGenerator = randomNumbers.reduce((acc, number) => acc.mockReturnValueOnce(number), jest.fn());
-
     const bridge = BridgeMaker.makeBridge(3, mockGenerator);
     expect(bridge).toEqual(['U', 'D', 'D']);
   });
-
   test('기능 테스트', async () => {
     const logSpy = getLogSpy();
     mockRandoms(['1', '0', '1']);
     mockQuestions(['3', 'U', 'D', 'U']);
-
     const app = new App();
     await app.play();
-
     const log = getOutput(logSpy);
     expectLogContains(log, [
       '최종 게임 결과',
@@ -76,7 +75,23 @@ describe('다리 건너기 테스트', () => {
     expectBridgeOrder(log, '[ O |   | O ]', '[   | O |   ]');
   });
 
-  test('예외 테스트', async () => {
-    await runException(['a']);
+  test('다리 길이에 3이상 20이하의 숫자가 입력되지 않을 경우 예외 처리한다.', async () => {
+    await runException(['a'], ERROR_MESSAGE.bridgeSize);
+    await runException(['1'], ERROR_MESSAGE.bridgeSize);
+    await runException(['21'], ERROR_MESSAGE.bridgeSize);
+  });
+
+  test('이동할 칸에 U 혹은 D가 입력되지 않을 경우 예외 처리한다.', async () => {
+    await runException(['3', 'u'], ERROR_MESSAGE.moving);
+    await runException(['3', 'd'], ERROR_MESSAGE.moving);
+    await runException(['3', 'UP'], ERROR_MESSAGE.moving);
+    await runException(['3', 'Down'], ERROR_MESSAGE.moving);
+  });
+
+  test('게임 재시작/종료 여부에 R 혹은 D가 입력되지 않을 경우 예외 처리한다.', async () => {
+    await runException(['3', 'U', 'U', 'D', 'r'], ERROR_MESSAGE.gameCommand);
+    await runException(['3', 'U', 'U', 'D', 'd'], ERROR_MESSAGE.gameCommand);
+    await runException(['3', 'U', 'U', 'D', 'Retry'], ERROR_MESSAGE.gameCommand);
+    await runException(['3', 'U', 'U', 'D', 'Done'], ERROR_MESSAGE.gameCommand);
   });
 });
